1c72f60efba9a590d6dccdbfa6bb3fa4881ec803
Leandro <leandrodericco@hotmailcom>
2015-05-17 13:28:49 -0300

    Entity

--- Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -8,7 +8,7 @@ namespace Quiron.LojaVirtual.Dominio.Entidade
 {
     public class Produto
     {
-        public int Produtoid { get; set; }
+        public int ProdutoId { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public decimal Preco { get; set; }
